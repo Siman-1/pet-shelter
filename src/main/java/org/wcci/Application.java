fd919be378b0da7fdc020ae8339ecac867ea8815
Marshall G. Flax <marshall@wecancodeit.org>
2023-08-01 10:24:38 -0400

    Minimal spring+react

---
+++ src/main/java/org/wcci/Application.java
@@ -0,0 +1,16 @@
+package org.wcci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@Component
+public class Application {
+    public static void main(String[] args) {
+        // First going to see if all requirements can be satisified
+        // Then it's going to create all components
+        // Let them run
+        SpringApplication.run(Application.class, args);
+    } 
+}
