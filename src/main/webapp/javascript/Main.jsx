ddad78fcc8bbc466fe419e449ee3d1cb595e464c
Siman Farah <zfarah23@gmail.com>
2023-08-02 10:07:02 -0400

    commit

---
+++ src/main/webapp/javascript/Main.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import "../css/styles.css";
+
+
+function Main() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/app1?/src?/main?/resources?/static?/index.html?" element={<Layout />}></Route>
+          <Route index element={<Layout />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+createRoot(document.getElementById("react-mountpoint")).render(<Main />);
