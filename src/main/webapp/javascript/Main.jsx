17072f0177855e8695bad21599ef817b98c5f77e
Siman Farah <zfarah23@gmail.com>
2023-08-04 11:49:05 -0400

    added co-creators images. added classes for images

--- src/main/webapp/javascript/Main.jsx
+++ src/main/webapp/javascript/Main.jsx
@@ -6,38 +6,34 @@ import Adopt from "./AdoptPet";
 import Shelter from "./Shelter";
 import Activities from "./Activities";
 
-
-
-
 function Layout() {
   return (
     <>
       <nav>
         <Link to="/">Main</Link>
         <Link to="./Shelter">Shelter</Link>
-        
       </nav>
       <h1>Welcome to our Humane Society! Click Shelter to interact with pets!</h1>
-      <h3> Created by:</h3>
-      <p1> Kai</p1>
-      <p2>Mckeon</p2>
-      <p3>Stephen</p3>
-      <p4> Siman</p4>
       <div class="animalShelter"></div>
-      <div class="Kai">
-        <img id="imageKai" class="Kai" src="images/blahblah" alt="imageKai" height="256px" width="256px" />
+      <div class="imageContainer">
+        <div class="kmss">
+          <p> Kai</p>
+          <img id="imageKai" class="kmss" src="images/Kai.png" alt="imageKai" height="75px" width="75px" />
+        </div>
+        <div class="kmss">
+          <p> Stephen</p>
+          <img id="imageStephen" class="kmss" src="images/Stephen.png" alt="imageStephen" height="75px" width="75px" />
+        </div>
+        <div class="kmss">
+          <p> Mckeon</p>
+          <img id="imageMckeon" class="kmss" src="images/Mckeon.png.png" alt="imageMckeon" height="75px" width="75px" />
+        </div>
+        <div class="kmss">
+          <p> Siman</p>
+          <img id="imageSiman" class="kmss" src="images/Siman.png" alt="imageSiman" height="75px" width="75px" />
+        </div>
       </div>
-      <div class="Stephen">
-        <img id="imageStephen" class="Stephen" src="images/blahblah" alt="imageStephen" height="256px" width="256px" />
-      </div>
-      <div class="Mckeon">
-        <img id="imageMckeon" class="Mckeon" src="images/blahblah" alt="imageMckeon" height="256px" width="256px" />
-      </div>
-      <div class="Siman">
-        // ask if this should match p1???
-        <img id="imageSiman" class="Siman" src="images/blahblah" alt="imageSiman" height="256px" width="256px" />
-      </div>
-
+      <h3> Created by:</h3>
       <Outlet />
     </>
   );
@@ -48,8 +44,8 @@ function Main() {
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path='/adopt' Component={Adopt}></Route>
-          <Route path = "Shelter" element={<Shelter />}></Route>
+          <Route path="/adopt" Component={Adopt}></Route>
+          <Route path="Shelter" element={<Shelter />}></Route>
           <Route index element={<Layout />}></Route>
         </Routes>
       </BrowserRouter>
